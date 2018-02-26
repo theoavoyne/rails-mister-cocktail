79e962d5f2f2ca1c970d12212ff2cc2d86873063
Theo Avoyne <tavoyne@gmail.com>
2018-02-25 22:06:22 -0500

    fix front-end bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,17 +13,3 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
- function find_addose() {
-  var adddose = document.getElementById("adddose");
-  var new_dose = document.getElementById("new_dose");
-  console.log(adddose);
-  console.log(new_dose);
-
-  adddose.addEventListener(("click"), (event) => {
-    event.currentTarget.style.display = "none";
-    new_dose.style.display = "flex";
-  })
-}
-
-find_addose()
-
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,13 @@
+function find_addose() {
+  var adddose = document.getElementById("adddose");
+  var new_dose = document.getElementById("new_dose");
+  console.log(adddose);
+  console.log(new_dose);
+
+  adddose.addEventListener(("click"), (event) => {
+    event.currentTarget.style.display = "none";
+    new_dose.style.display = "flex";
+  })
+}
+
+find_addose()
